5f48be91d1a65d608bedf71fe8d8350ba6b3b9a6
Sandro Roth <sandroroth@outlook.com>
2021-10-23 17:49:02 +0200

    Update to angular 12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { DemoOneComponent } from './demo-one.component';
 import { DemoTwoComponent } from './demo-two.component';
